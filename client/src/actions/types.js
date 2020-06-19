fd635fb9cecd82e0cfa0d082752980682e1288a5
adrian mustea <mustea.adrian95@gmail.com>
2020-06-19 23:13:14 +0300

    wip

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,6 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
+export const CREATE_PROFILE = "CREATE_PROFILE";
+export const EDIT_PROFILE = "EDIT_PROFILE";
+export const DELETE_PROFILE = "DELETE_PROFILE";
